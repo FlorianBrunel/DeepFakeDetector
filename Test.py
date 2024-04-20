48a7cda837cae98a929dcedbcb8faf388008278f
FlorianBrunel <florian.brunel@viacesi.fr>
2024-04-20 07:00:47 +0200

    new decomposition script

--- Test.py
+++ Test.py
@@ -4,7 +4,6 @@ import tkinter as tk
 from tkinter import filedialog
 
 def lister_chemins_dossier(dossier):
-    print("Test")
     for chemin, dossiers, fichiers in os.walk(dossier):
         for fichier in fichiers:
             yield os.path.join(chemin, fichier)
@@ -13,12 +12,12 @@ def decomposer_video_en_images(chemin_video, dossier_sortie):
     video = cv2.VideoCapture(chemin_video)
     succes, image = video.read()
     compteur = 0
+    nom_video = os.path.basename(chemin_video).split('.')[0]  # Ajouté
     while succes:
-        cv2.imwrite(os.path.join(dossier_sortie, "frame%d.jpg" % compteur), image)
+        cv2.imwrite(os.path.join(dossier_sortie, f"{nom_video}_frameV2{compteur}.jpg"), image)  # Modifié
         succes, image = video.read()
         compteur += 1
 
-print("Test")
 root = tk.Tk()
 root.withdraw()
 
@@ -26,6 +25,7 @@ dossier = filedialog.askdirectory()  # Demande à l'utilisateur de sélectionner
 
 for chemin_fichier in lister_chemins_dossier(dossier):
     if chemin_fichier.endswith(('.mp4', '.avi', '.mov')):  # Ajoutez d'autres extensions de vidéo si nécessaire
-        dossier_sortie = os.path.join(os.path.dirname(chemin_fichier), os.path.basename(chemin_fichier) + "_frames")
+        dossier_sortie = "DataSetVideo/Fake"
         os.makedirs(dossier_sortie, exist_ok=True)
         decomposer_video_en_images(chemin_fichier, dossier_sortie)
+        print("En cours")
